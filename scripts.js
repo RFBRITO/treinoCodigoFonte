5f96f801a227a834f175835f645a9fc925e1c39a
Rafael Brito <rafaelduarte231q@gmail.com>
2025-06-10 22:43:00 -0300

    Adicionei função escolher curso

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 function entrarNocurso(){
     console.log('Entrou no curso')
 }
+
 function sairdocurso() {
     console.log("Saiu do curso")
+}
+
+function escolhercurso() {
+    console.log('Escolheu o curso')
 }
